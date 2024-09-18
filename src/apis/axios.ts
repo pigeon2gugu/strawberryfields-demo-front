be4623c6ab146856f5e1fdfd359393ac88de2ec4
pigeon2gugu <khn1135@gmail.com>
2024-09-19 01:00:36 +0900

    feat : accessToken 만료 시 재발급 로직 추가

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,4 +1,13 @@
 import axios from 'axios';
+import { API_ENDPOINTS } from '@/constants/Endpoints';
+
+let accessToken: string | null = null;
+
+export const setAccessToken = (token: string) => {
+  accessToken = token;
+};
+
+export const getAccessToken = () => accessToken;
 
 const config = {
   baseURL: 'http://localhost:8080/api/v1',
@@ -13,9 +22,8 @@ const apiClient = axios.create(config);
 // Request interceptor
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('authToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
     }
     return config;
   },
@@ -24,15 +32,50 @@ apiClient.interceptors.request.use(
   }
 );
 
+// Refresh Token을 통해 Access Token 재발급 함수
+const refreshAccessToken = async () => {
+  const refreshToken = localStorage.getItem('refreshToken');
+
+  if (!refreshToken) {
+    throw new Error('No refresh token found');
+  }
+
+  const response = await axios.get(API_ENDPOINTS.AUTH.REFRESH, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      refreshToken: ` ${refreshToken}`,
+    },
+  });
+
+  accessToken = response.data.accessToken;
+  localStorage.setItem('refreshToken', response.data.refreshToken);
+
+  return accessToken;
+};
+
 // Response interceptor
 apiClient.interceptors.response.use(
   (response) => {
     return response;
   },
-  (error) => {
-    if (error.response?.status === 401) {
-      // Handle unauthorized error (e.g., redirect to login)
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.data?.code === 'EXPIRED_ACCESS_TOKEN') {
+      try {
+        const newAccessToken = await refreshAccessToken();
+        accessToken = newAccessToken;
+        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+
+        return apiClient(originalRequest);
+      } catch (refreshError) {
+        console.error('Refresh token failed or expired.', refreshError);
+        localStorage.removeItem('refreshToken');
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      }
     }
+
     return Promise.reject(error);
   }
 );
